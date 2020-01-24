f0f0f9cc2d7669e6f2baf24de5f5a8248e97e6e1
markpayne777 <markpayne93@gmail.com>
2020-01-24 23:47:24 +0200

    Solution to exercise in chapter3

--- src/test/java/Base/BaseTests.java
+++ src/test/java/Base/BaseTests.java
@@ -1,7 +1,11 @@
 package Base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
@@ -9,12 +13,24 @@ public class BaseTests {
     public static void main(String[] args) {
         BaseTests test = new BaseTests();
         test.setUp();
+        test.exerciseChapter3();
+        test.tearDown();
     }
 
     public void setUp() {
         System.setProperty("webdriver.gecko.driver", "src//main//resources//geckodriver.exe");
         driver = new FirefoxDriver();
-        driver.get("https://google.com");
-        System.out.println(driver.getTitle());
+    }
+
+    public void exerciseChapter3() {
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("/html/body/div[2]/div/ul/li[38]/a")).click();
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+        List<WebElement> elements = driver.findElements(By.tagName("li"));
+        System.out.println("There are: " + elements.size() + " elements on the page");
+    }
+
+    public void tearDown() {
+        driver.quit();
     }
 }
